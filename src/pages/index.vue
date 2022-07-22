f9481bb6c6199a9ff4bfff434f565886e776fb0e
Xie Yuheng <xyheme@gmail.com>
2022-07-22 08:26:08 +0800

    up

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,12 +4,14 @@ const { data: users } = await useFetch(url)
 </script>
 
 <template>
-  <div class="text-3xl">Hello World!</div>
+  <div>
+    <div class="text-3xl">Hello World!</div>
 
-  <ul class="text-3xl py-3">
-    <li v-for="user of users" :key="user.id">
-      <div>{{ user.username }}</div>
-      <div>{{ user.name }}</div>
-    </li>
-  </ul>
+    <ul class="text-3xl py-3">
+      <li v-for="user of users" :key="user.id">
+        <div>{{ user.username }}</div>
+        <div>{{ user.name }}</div>
+      </li>
+    </ul>
+  </div>
 </template>
