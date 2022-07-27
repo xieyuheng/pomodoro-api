1b8eba3f819603ecf7cbe95925827208fd94e3e5
Xie Yuheng <xyheme@gmail.com>
2022-07-28 07:22:57 +0800

    up

---
+++ k.sh
@@ -0,0 +1,2 @@
+docker rm -f redis-stack-server
+docker run -d -v /home/xyh/redis/data/:/data -v /home/xyh/redis/redis.conf:/redis-stack.conf --name redis-stack-server -p 6379:6379 redis/redis-stack-server:latest
