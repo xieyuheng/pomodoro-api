59f0e683f437f6ad28e89da8455e1fd989355367
Xie Yuheng <xyheme@gmail.com>
2022-08-01 20:18:04 +0800

    up

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,7 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const auth = useAuth()
+  if (auth.user) return
+
   await auth.loadUser()
   if (!auth.user) {
     return navigateTo("/", { replace: true })
