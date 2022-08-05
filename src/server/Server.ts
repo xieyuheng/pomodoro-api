762b55fd6bb1e39770ced1e47bf6c1e95898fc31
Xie Yuheng <xyheme@gmail.com>
2022-08-05 09:58:16 +0800

    up

---
+++ src/server/Server.ts
@@ -0,0 +1,23 @@
+import { Router } from "express"
+import express, { Application } from "express"
+
+export class Server {
+  router: Router
+  expressApp: Application
+
+  constructor(opts: { router: Router }) {
+    this.router = opts.router
+    this.expressApp = express()
+  }
+
+  async run(opts: { port: number; host: string }): Promise<void> {
+    const { port, host } = opts
+
+    this.expressApp
+      .use(this.router)
+      .disable("x-powered-by")
+      .listen({ port, host })
+
+    console.log({ who: "Server.run", host, port })
+  }
+}
