5b1b4cf81d748b178e08bce035c37ba5f3dacbca
Xie Yuheng <xyheme@gmail.com>
2022-08-05 10:07:33 +0800

    up

--- src/run.ts
+++ src/run.ts
@@ -5,8 +5,16 @@ export async function run(): Promise<void> {
   const router = Router()
   const server = new Server({ router })
 
+  router.get("*", (req, res) => {
+    res.setHeader("Content-Type", "text/html")
+    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate")
+    res.end("Hello world!")
+  })
+
   await server.run({
     port: Number(process.env.PORT),
     host: process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1",
   })
 }
+
+run().catch(console.error)
