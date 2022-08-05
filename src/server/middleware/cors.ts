c15fd74d16061d1d50cfe3a5fcfa813d4700c16f
Xie Yuheng <xyheme@gmail.com>
2022-08-05 08:02:14 +0800

    up

--- src/server/middleware/cors.ts
+++ src/server/middleware/cors.ts
@@ -1,5 +1,5 @@
 import { AccessTokenController } from "@/server/controllers/AccessTokenController"
 
 export default defineEventHandler(async (event) => {
-  event.req.setHeader("Access-Control-Allow-Origin", "*")
+  event.res.setHeader("Access-Control-Allow-Origin", "*")
 })
