ccc31e8de65224ce020fc51dc974484ddc42c221
Xie Yuheng <xyheme@gmail.com>
2022-08-06 19:12:22 +0800

    up

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,7 +2,6 @@ import { Controller } from "../framework/routing/Controller"
 
 export class UserController extends Controller {
   async current() {
-    console.log("auth:", this.router.express.get("auth"))
     const user = this.router.express.get("auth")?.user
     if (!user) {
       this.res.status(404).end()
